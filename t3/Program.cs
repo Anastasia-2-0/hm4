fc1d89565340d84f81439055799e58715b4d0324
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-12 09:22:55 +0300

    решение 3 задачи

--- t3/Program.cs
+++ t3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+ void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,100);
+        index++;
+    }
+}
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(",");
+        position++;
+    }
+}
+
+int[] array =new int[10];
+
+FillArray(array);
+PrintArray(array);
