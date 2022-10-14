b80ba540fdb80f1d0fb15ef2fbb1fb167b92a319
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-14 13:13:17 +0300

    решение задач

--- t1/Program.cs
+++ t1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+Console.WriteLine("Введите число первое: ");
+int numberOne=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число второе: ");
+int numberTwo=int.Parse(Console.ReadLine());
+Console.Write($"{numberOne} в степени {numberTwo} = {LevelNumber(numberOne,numberTwo)}");
+ 
+int LevelNumber(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+   return result;
+}
+
+
--- t2/Program.cs
+++ t2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"сумма чисел = {SumNumber(number)}");
+
+int SumNumber(int n )
+{
+int sum = 0;
+int a = 0;
+while( n > 0)
+{
+    a = n % 10;
+    sum += a;
+    n = n / 10;
+}
+return sum;
+}
+
+
